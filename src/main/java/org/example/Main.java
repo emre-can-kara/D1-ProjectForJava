80622355ac18cd9e56f2b4e595d7f63ec470a6f2
Emre Can Kara <emre_can_kara@outlook.com>
2025-03-03 21:16:51 +0300

    All assignments have been done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,10 +30,16 @@ public class Main {
     }
 
     public static double area(double width, double height) {
-       
+       if (width < 0 || height < 0) {
+           return -1;
+       }
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+       if (radius < 1) {
+           return -1;
+       }
+       return radius*radius*Math.PI;
     }
 }
